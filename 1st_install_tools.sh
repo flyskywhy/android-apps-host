0ec2c71425ac64fdd77fd59b8a3641a6e2204071
Li Zheng <flyskywhy@gmail.com>
2012-05-22 16:39:55 +0800

    Auto install the needed tools

---
+++ 1st_install_tools.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+# Author: Li Zheng <flyskywhy@gmail.com>
+
+apt-get install autopoint cmake << EOF
+Y
+EOF
+
+mv /usr/share/misc/config.guess /usr/share/misc/config.guess.origin
+cp ../../tools/config/config.guess /usr/share/misc/config.guess
+mv /usr/share/misc/config.sub /usr/share/misc/config.sub.origin
+cp ../../tools/config/config.sub /usr/share/misc/config.sub
