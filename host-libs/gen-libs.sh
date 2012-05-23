cb96f520a97d6bec5eac2daf298b9fd77a378bed
Li Zheng <flyskywhy@gmail.com>
2012-05-23 13:05:59 +0800

    Add a global variable HOST_LIBS to support LOCAL_STATIC_LIBRARIES in Android.mk

---
+++ host-libs/gen-libs.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+function git_and_build()
+{
+    [ ! -d ../../$1/.git ] && git clone .. -b $1-gb ../../$1
+    cd ../../$1
+    ./compile.sh
+    cd -
+    cp ../../$1/vlc-android/libs/host/$1.a ./
+}
+
+#echo $(git_and_build libcutils)
