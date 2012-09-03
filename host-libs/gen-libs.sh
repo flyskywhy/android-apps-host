6c94c10440a2609ed0c513b49665e36e2e8065c3
Li Zheng <flyskywhy@gmail.com>
2012-09-03 10:40:06 +0800

    If new clone, can't clone in host-libs/gen-libs.sh with remote branch, fix it.

--- host-libs/gen-libs.sh
+++ host-libs/gen-libs.sh
@@ -2,11 +2,13 @@
 
 function git_and_build()
 {
-    [ ! -d ../../$1/.git ] && git clone .. -b $1-gb ../../$1
+    [ ! -d ../../$1/.git ] && git branch $1-gb origin/$1-gb
+    git clone .. -b $1-gb ../../$1
     cd ../../$1
     ./compile.sh
     cd -
     cp ../../$1/vlc-android/libs/host/$1.a ./
+    [ -d ../../$1/.git ] && git branch -d $1-gb
 }
 
 #echo $(git_and_build libcutils)
